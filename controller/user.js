371bcb65e169dea8d50e82634a0c6c071104d42c
kouame konan abraham <abrahamkonany@gmail.com>
2023-10-09 14:20:38 +0000

    mise à jour des api

--- controller/user.js
+++ controller/user.js
@@ -19,7 +19,7 @@ class UtilisateurController{
                     res.status(202).json({
                         status:true,
                         token: generateToken(newUser.toObject()),
-                        message : "Compte crée !!!!"
+                        message : "Compte crée Merci  !!!!"
                     })
                     req.cookie("token", generateToken(newUser.toObject()))
                 })
