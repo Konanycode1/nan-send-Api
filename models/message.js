0b7b338a380ae5b95df1181f02318662901cc9c9
nfcdjobo <nfcdjobo@gmail.com>
2023-11-19 06:20:46 +0000

    m

--- models/message.js
+++ models/message.js
@@ -23,8 +23,8 @@ const Message = new Schema ({
 
     accuse:{ type: Boolean, default : false, required: true },
     statut:{ type: Number, default: 1, required: true },
-    createdAt:{ type: Date, required: true, default: Date.now() },
-    updatedAt:{ type: Date, required: true, default: Date.now() }
+    createdAt:{ type: Date, required: true, default: Date.now },
+    updatedAt:{ type: Date, required: true, default: Date.now }
 },
 {
     timesTamps: true
