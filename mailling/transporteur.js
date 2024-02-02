ca724af4af013447e066257ea19d1e18b23aa44a
nfcdjobo <nfcdjobo@gmail.com>
2024-02-02 01:54:55 +0000

    Last commit

--- mailling/transporteur.js
+++ mailling/transporteur.js
@@ -1,18 +1,41 @@
+// import nodemailer from "nodemailer";
+// const transporteur =  auth =>{
+//     const connectivite = !auth.user.includes('@outlook') ?
+//     {
+//         service: 'gmail',
+//         auth:auth,
+//         debug:true,
+//     } : 
+//     {
+//         host: 'smtp.office365.com',
+//         port: 587,
+//         secureConnection:false,
+//         tls:{ciphers: "SSLv3"},
+//         auth:auth,
+//         debug:true,
+//     };
+//     return nodemailer.createTransport(connectivite);
+// };
+// export default transporteur;
+
 import nodemailer from "nodemailer";
-const transporteur =  auth =>{
-    const connectivite = !auth.user.includes('@outlook') ?
-    {
-        service: 'gmail'
-    } : 
-    {
-        host: 'smtp.office365.com',
-        port: 587,
-        secureConnection:false,
-        tls:{ciphers: "SSLv3"},
+
+const transporteur = (auth) => {
+    const connectivite = {
+        auth,
+        debug: true,
     };
-    connectivite.debug = true;
-    connectivite.auth = auth;
+
+    if(!auth.user.includes('@outlook')) {
+        connectivite.service = 'gmail';
+    }else{
+        connectivite.host = 'smtp.office365.com';
+        connectivite.port = 587;
+        connectivite.secureConnection = false;
+        connectivite.tls = { ciphers: "SSLv3" };
+    }
+
     return nodemailer.createTransport(connectivite);
-    // return nodemailer.createTransport({host:"smtp-mail.outlook.com", port:587, secureConnection:false, tls:{ciphers:"SSLv3"}, auth:auth})
 };
+
 export default transporteur;
